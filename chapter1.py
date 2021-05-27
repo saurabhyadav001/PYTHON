fed65b6b563e4c5578a6d9e8908234b5a1a414a5
SAURABH YADAV <72190873+saurabhyadav3615@users.noreply.github.com>
2021-05-27 14:55:20 +0530

    Add files via upload

---
+++ chapter1.py
@@ -0,0 +1,103 @@
+        #********* BASICS************
+# we can not use variables(1,2,3,4) in starting of an statement.
+# we can not use special symbules in starting or in any place of the statement(@,$,%.....)
+# user_one is = "saurabh" THIS IS SNAKE CASE WRITING|
+# UserOne Is ="saurabh"  THIS IS CAMEL CASE WRITING|
+
+
+
+# HELLOW WORLD PROGRAM;**************
+
+print("this is saurabh yadav\nfrom JIET college")   # use of next line
+print("/\\/\\/\\")     # structure of an mountain
+# print("123"+"abc")   # for providing more space
+# x=42%10
+# print(x)
+
+            # *********** COMMENTS////*******
+
+# print("saurabh\nyadav") #\n is for the new line                                      
+# print("saurabh\tyadav") #\t is for the new tab or for more distaance between the two words
+                  # the aforsaid lines in the hashtag is called as comment.
+                
+
+      #******HOW TO PRINT AN EMOZI**********;
+
+# print("\U0001F923")    #to know the codes of the particular emozi u have to search on unicode emozi code copy
+
+# print("\U0001f601")
+
+
+    # ***** Int/float string*************
+# number1=int(str(5))
+# number2=int(str(8))
+# print(number1+number2)
+# number1=int(5)
+# number2=float(8)
+# print(number1+number2)
+
+
+            #*********PYTHON AS AN CALCULATOR**********
+
+# print(2+4)      #in python for the mathematical calculations we do not use the inverted commas
+# print(2+8*3)
+# print(2*5*8+5-2)
+
+
+
+      #for adding numbers like calculator***********
+
+# number_one = int(input("enter any number"))             
+# number_two = int(input("enter any number"))
+# total =(number_one + number_two)
+# print((total)) or  print(float(number_one+number_two))
+
+
+# number_one=int(input("enter a number"))      // practice ques.
+# number_two=int(input("enter a number"))
+# number_three=int(input("enter a number"))
+# print((number_one+number_two+number_three)/3)
+
+
+
+            #  NORMAL STRING*******************
+
+# FirstName="saurabh"                      #program to add two diffrent lines program
+# LastName= "yadav"                      # this is written in camel case
+# FullName=(FirstName +  LastName)        
+# FullName=(FirstName+"   "+LastName)     #" " this symbol used for space  
+# print(FullName)
+# first_name ="hellow"                #this is written in snake case
+# last_name="world"
+# # full_name=first_name+3    #here we cannot add any number ! error
+# full_name=first_name+"3"   #no error
+# # full_name=first_name*  3       # we can multipy {NO ERROR}
+# print(full_name)
+
+
+
+      # ************MORE ABOUT VARIABLES**********
+
+# name,age=" saurabh","19"
+# print("hii"+ name+" your age is"+age)
+# name,age="saurabh","19"         #direct method
+# print(name)
+# print(age)
+#    ****** # alternate for pyhton 3.6 users
+#  name="saurabh"
+#  age= 19                                        
+#  print(f"hello{name} your age is{age}")     
+
+
+            # PASS STATEMENT***********
+#  x= 15           # PASS statement is used to remove the syntex errors
+# if x15:
+# pass
+
+        #  SCAPES************
+# print("saurabh\nyadav")
+# print('saurabh\t\tyadav')
+# print("this is double backslash\\\\")
+# print("this is backslash\\ ")
+# print("line a \\n line b") #\\backslash will make a single backslash 
+# print("\\'\\'")
