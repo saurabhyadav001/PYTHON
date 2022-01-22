a7c8f82af58fbcdd766b890e23efcb0b2eb359c7
SAURABH YADAV <72190873+saurabhyadav3615@users.noreply.github.com>
2022-01-22 23:29:05 +0530

    Create Comp_PYTHON01.py

---
+++ Comp_PYTHON01.py
@@ -0,0 +1,71 @@
+# print("saurabh")
+# print('yadav') 
+#in python we can use single as well as double code too represent an string.
+
+# print("saurabh 'yadav' cse ") #we can use double or in-between single as well as single code in between double.
+# print('saurabh "yadav" cse') #as well as we can't use same codes in between same codes .
+
+#TUTORIal 10 ----*********ESCAPES SEQUENCES*********
+
+#*****In Python, escape sequences are indicated by a backslash ( \ ). The most important one may be \n which indicates a new line. ... 
+# Another escape sequence worth mentioning is \' for a single quote within a string. A backslash also escapes itself so if it's really a backslash you want,
+# double it as in*** \\ .
+
+#using backslash '\' avoids giving syntax error when we use same codes between same codes.
+#  \' ------- Single quote , \"--Double Quote , \\-- backlash, \n, \t, \b--backspace.
+# print ('saurabh \n yadav \n cse ')
+# print('this is backslash\\ \n this is doubele backslash \\\\')
+# print('Line A \\n Line B')   #**Escapes Sequences As a Normal Text.**
+# print("\\\" \\\'")
+#EXERCISE 01********
+# print(' these are /\\/\\/\\/\\ mountains')
+# print ('he is \t awesome')
+# print('\\\"\\\' \\')
+
+#***RAW STRING --- USING RAW STRING WE CAN TREAT AN ESCAPE SEQUENCE AS A NORAMAL TEXT.
+#eX......
+# print(r'line A \" line B')
+
+#PRINT EMOJI********WEB--- UNICODE.ORG
+# print('\U0001F602')  #just replace '+' sign with '000'!
+
+#PYTHON as a calculator
+
+# print(5+6)
+# print(2*2*4)
+# print(2**2**3)
+# print(2/4)  #floating point devision.
+# print (4//2) #integer devision. 
+# print(2**3) #exponential  , ie. 2 to the power 3!
+# print(16**0.5) #Underroot.
+# print(2**3/2*6-4)
+#PRESENDENce RULE
+# Operators	Meaning
+# ()	Parentheses
+# **	Exponent
+# +x, -x, ~x	Unary plus, Unary minus, Bitwise NOT
+# *, /, //, %	Multiplication, Division, Floor division, Modulus
+# +, -	Addition, Subtraction
+# <<, >>	Bitwise shift operators
+# &	Bitwise AND
+# ^	Bitwise XOR 
+# |	Bitwise OR
+# ==, !=, >, >=, <, <=, is, is not, in, not in	Comparisons, Identity, Membership operators
+# not	Logical NOT
+# and	Logical AND
+# or	Logical OR
+
+
+#***************VARAIABLES*******
+#python is dynamic programming lang, here we do not need to declare that any variable type ex.
+#int , char , float etc.. so, python gives flexibily in terms of declaring variable types.
+
+# name = 'saurabh'
+# print(name)
+
+# name = 12345
+# print(name)
+#variable name can't start with any number or any special character.
+ 
+# user_one_name= "saurabh"    #snake case writing  **recommended for PYTHO
+# userOneName= "Yadav"  # camel case writing.
