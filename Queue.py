85e08c447b46d2d8f09ef2407d9877e7215f2308
SAURABH YADAV <72190873+saurabhyadav3615@users.noreply.github.com>
2022-01-22 23:27:15 +0530

    Create Queue.py

---
+++ Queue.py
@@ -0,0 +1,28 @@
+# def enque():
+#     element= input('enter the element')
+#     queue.append(element)
+#     print(queue)
+# def deque():
+#     if not queue:
+#         print("queue is empty")
+#     else:
+#         e= queue.pop()
+#         print("popped element is",e)
+#         print(queue)
+# def display():
+#     print(queue)
+
+# queue= []
+# while True:
+#     print("enter your choice 1.enque 2.dewue 3.display 4.quite")
+#     choice = int(input())
+#     if choice ==1:
+#         enque()
+#     elif choice ==2:
+#         deque()
+#     elif choice ==3:
+#         display()
+#     elif choice ==4:
+#         break
+#     else:
+#         print("invalid enterred criteria")
